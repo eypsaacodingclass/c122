807734576c131e8b4854fee60bab29ab6b196260
eypsaacodingclass <100167688+eypsaacodingclass@users.noreply.github.com>
2022-11-09 17:19:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+ screen_width = window.innerWidth;
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
+
+function preload() {
+  loadImage = apple.png
+}
+
+function Number() {
+  to_number = Number(content);
+  if(Number.isInteger(to_number))
+}
+
+function screen_hight() {
+  screen_height = window.innerHeight;
+}
+
+createCanvas() {
+screen_width = 0
+screen_hight = 150
+
+canvas.position() (0,150)
+
